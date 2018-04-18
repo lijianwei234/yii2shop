d1c67e005da2d6f37777200d9ef3edb530f1f2e7
lijianwei <1311274818@qq.com>
2018-04-18 18:30:04 +0800

    再次测试

--- web/index.php
+++ web/index.php
@@ -1,5 +1,5 @@
 <?php
-# this from desktop1 
+# this from desktop8
 // comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
